91863bfad09086d75e830e8256552018c4e3ac58
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-06-01 18:31:08 +0200

    up

--- 268.missing-number.js
+++ 268.missing-number.js
@@ -9,8 +9,11 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    return 1;
+var missingNumber = function (nums) {
+  let length = nums.length,
+    sum = (length * (length + 1)) / 2;
+  nums.forEach((i) => (sum -= i));
+  return sum;
 };
 // @lc code=end
-
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 7, 9]));
